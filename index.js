d753439939e110ab7e67351c483e2679f12fcecc
Viraj-10 <virajjoshi132@gmail.com>
2022-06-15 23:39:24 +0530

    basic working done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const fs = require("fs");
+const envinfo = require("envinfo");
+const { platform } = require("os");
+
+let packages = [
+  "react",
+  "react-native",
+  "native-base",
+  "react-dom",
+  "react-native-svg",
+  "react-native-safe-area-context",
+  "@react-native-community/cli",
+  "react-native-web",
+  "expo",
+];
+
+function getInfo() {
+  try {
+    envinfo
+      .run({
+        System: ["OS", "CPU", "Memory", "Shell"],
+        Binaries: ["Node", "Yarn", "npm", "Watchman"],
+        IDEs: ["Xcode", "Android Studio", "Visual Studio"],
+        SDKs: ["iOS SDK", "Android SDK", "Windows SDK"],
+        npmPackages: packages,
+        npmGlobalPackages: ["*react-native*"],
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getInfo();
